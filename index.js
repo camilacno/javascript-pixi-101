c56f2ae9dcc3ad33e729be46ee4e06645441ef78
Camila Nepomuceno <camila.cno@gmail.com>
2021-09-28 21:09:13 -0300

    Refactoring

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 import * as PIXI from 'pixi.js';
 import Victor from 'victor';
-//import Matter from "matter-js";
+import Player from './player.js';
+import Zombie from './zombie.js';
 
 const canvasSize = 256;
 const canvas = document.getElementById('mycanvas');
@@ -11,71 +12,10 @@ const app = new PIXI.Application({
   backgroundColor: 0x5c812f,
 });
 
-const squareWidth = 32;
-const square = new PIXI.Sprite(PIXI.Texture.WHITE);
-square.anchor.set(0.5);
-square.position.set(app.screen.width / 2, app.screen.height / 2);
-square.width = square.height = squareWidth;
-square.tint = 0xea985d;
-
-app.stage.addChild(square);
-
-const enemyRadius = 16;
-const enemySpeed = 2;
-const enemy = new PIXI.Graphics();
-let r = randomSpawnPoint();
-enemy.position.set(r.x, r.y);
-enemy.beginFill(0xff0000, 1);
-enemy.drawCircle(0, 0, enemyRadius);
-enemy.endFill();
-app.stage.addChild(enemy);
+let player = new Player({ app });
+let zombie = new Zombie({ app, player });
 
 app.ticker.add((delta) => {
-  const cursorPosition = app.renderer.plugins.interaction.mouse.global;
-  let angle =
-    Math.atan2(
-      cursorPosition.y - square.position.y,
-      cursorPosition.x - square.position.x
-    ) +
-    Math.PI / 2;
-  square.rotation = angle;
-
-  let enemyPosition = new Victor(enemy.position.x, enemy.position.y);
-  let squarePosition = new Victor(square.position.x, square.position.y);
-  if (enemyPosition.distance(squarePosition) < squareWidth / 2) {
-    let restartPosition = randomSpawnPoint();
-    enemy.position.set(restartPosition.x, restartPosition.y);
-    return;
-  }
-
-  let distance = squarePosition.subtract(enemyPosition);
-  let velocity = distance.normalize().multiplyScalar(enemySpeed);
-  enemy.position.set(
-    enemy.position.x + velocity.x,
-    enemy.position.y + velocity.y
-  );
+  player.update();
+  zombie.update();
 });
-
-function randomSpawnPoint() {
-  let edge = Math.floor(Math.random() * 4); // random int between 0 and 3 inclusive
-  let spawnPoint = new Victor(0, 0);
-  switch (edge) {
-    case 0: // top
-      spawnPoint.x = canvasSize * Math.random();
-      break;
-    case 1: // right
-      spawnPoint.x = canvasSize;
-      spawnPoint.y = canvasSize * Math.random();
-      break;
-    case 2: // botttom
-      spawnPoint.x = canvasSize * Math.random();
-      spawnPoint.y = canvasSize;
-      break;
-    default:
-      //left
-      spawnPoint.x = 0;
-      spawnPoint.y = canvasSize * Math.random();
-      break;
-  }
-  return spawnPoint;
-}
---
+++ zombie.js
@@ -0,0 +1,70 @@
+import * as PIXI from 'pixi.js';
+import Victor from 'victor';
+
+export default class Zombie {
+  constructor({ app, player }) {
+    this.app = app;
+    this.player = player;
+
+    const radius = 16;
+    this.speed = 2;
+    this.zombie = new PIXI.Graphics();
+    let r = this.randomSpawnPoint();
+
+    this.zombie.position.set(r.x, r.y);
+    this.zombie.beginFill(0xff0000, 1);
+    this.zombie.drawCircle(0, 0, radius);
+    this.zombie.endFill();
+
+    app.stage.addChild(this.zombie);
+  }
+
+  update() {
+    let zombiePosition = new Victor(
+      this.zombie.position.x,
+      this.zombie.position.y
+    );
+    let playerPosition = new Victor(
+      this.player.position.x,
+      this.player.position.y
+    );
+    if (zombiePosition.distance(playerPosition) < this.player.width / 2) {
+      let restartPoint = this.randomSpawnPoint();
+      this.zombie.position.set(restartPoint.x, restartPoint.y);
+      return;
+    }
+
+    let distance = playerPosition.subtract(zombiePosition);
+    let velocity = distance.normalize().multiplyScalar(this.speed);
+    this.zombie.position.set(
+      this.zombie.position.x + velocity.x,
+      this.zombie.position.y + velocity.y
+    );
+  }
+
+  randomSpawnPoint() {
+    let edge = Math.floor(Math.random() * 4); // random int between 0 and 3 inclusive
+    let spawnPoint = new Victor(0, 0);
+    let canvasSize = this.app.screen.width;
+
+    switch (edge) {
+      case 0: // top
+        spawnPoint.x = canvasSize * Math.random();
+        break;
+      case 1: // right
+        spawnPoint.x = canvasSize;
+        spawnPoint.y = canvasSize * Math.random();
+        break;
+      case 2: // botttom
+        spawnPoint.x = canvasSize * Math.random();
+        spawnPoint.y = canvasSize;
+        break;
+      default:
+        //left
+        spawnPoint.x = 0;
+        spawnPoint.y = canvasSize * Math.random();
+        break;
+    }
+    return spawnPoint;
+  }
+}
