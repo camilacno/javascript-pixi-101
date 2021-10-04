e5fb114d61a4f9a33b37f42d5d9eabace8bee6b1
Camila Nepomuceno <camila.cno@gmail.com>
2021-10-04 10:20:04 -0300

    Create health bar

--- player.js
+++ player.js
@@ -4,18 +4,45 @@ import Shooting from './shooting';
 export default class Player {
   constructor({ app }) {
     this.app = app;
+
     const playerWidth = 32;
     this.player = new PIXI.Sprite(PIXI.Texture.WHITE);
-
     this.player.anchor.set(0.5);
     this.player.position.set(app.screen.width / 2, app.screen.height / 2);
     this.player.width = this.player.height = playerWidth;
     this.player.tint = 0xea985d;
-
     app.stage.addChild(this.player);
 
     this.lastMouseButton = 0;
     this.shooting = new Shooting({ app, player: this });
+    //health bar
+    const margin = 16;
+    const barHeight = 8;
+    this.maxHealth = 100;
+    this.currentHealth = this.maxHealth;
+    this.healthBar = new PIXI.Graphics();
+    this.healthBar.beginFill(0xff0000);
+    this.healthBar.initialWidth = app.screen.width - 2 * margin;
+    this.healthBar.drawRect(
+      margin,
+      app.screen.height - barHeight - margin / 2,
+      this.healthBar.initialWidth,
+      barHeight
+    );
+    this.healthBar.endFill();
+    this.healthBar.zIndex = 1;
+    this.app.stage.sortableChildren = true;
+    this.app.stage.addChild(this.healthBar);
+  }
+
+  attack() {
+    this.health -= 1;
+    this.healthBar.width =
+      (this.health / this.maxHealth) * this.healthBar.initialWidth;
+
+    if (this.health <= 0) {
+      this.dead = true;
+    }
   }
 
   get position() {
